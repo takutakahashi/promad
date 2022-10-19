31dda01d2f57919afee62f672be389fb32c04a75
takutaka <takutaka@pepabo.com>
2022-10-20 00:23:39 +0900

    to_exporter

--- lib/lib.py
+++ lib/lib.py
@@ -5,15 +5,19 @@ import requests
 import json
 import pandas as pd
 import yaml
+import pmdarima as pm
+
 
 def env_defined():
     os.environ.get("PROM_API_URL") and \
-    os.environ.get("QUERY") and \
-    os.environ.get("ARIMA_ORDER") and \
-    os.environ.get("ARIMA_SEAZONAL_ORDER")
+        os.environ.get("QUERY") and \
+        os.environ.get("ARIMA_ORDER") and \
+        os.environ.get("ARIMA_SEAZONAL_ORDER")
+
 
 def arima_orders():
     return literal_eval(os.environ.get("ARIMA_ORDER")), literal_eval(os.environ.get("ARIMA_SEASONAL_ORDER"))
+
 
 def query_current():
     prom_api_url = os.environ.get("PROM_API_URL")
@@ -23,21 +27,25 @@ def query_current():
     data = json.loads(res.content)
     return data["data"]["result"][0]["value"]
 
+
 def val_to_series(values):
     y = [v[1] for v in values]
     x = [datetime.datetime.fromtimestamp(v[0]) for v in values]
     return pd.Series(y, index=x)
 
+
 def query_range(query):
     prom_api_url = os.environ.get("PROM_API_URL")
     query_range = prom_api_url + "/query_range"
-    end = int(datetime.datetime.now().timestamp() - 60 * 60 * 3)
-    start = datetime.datetime.now().timestamp() - 60 * 60 * 24 * 7
+    end = int(datetime.datetime.now().timestamp() - 60 * 60 * 1)
+    start = datetime.datetime.now().timestamp() - 60 * 60 * 24 * 3
     step = 3600
-    res = requests.get(query_range + "?query={}&start={}&end={}&step={}".format(query, start, end, step))
+    res = requests.get(
+        query_range + "?query={}&start={}&end={}&step={}".format(query, start, end, step))
     data = json.loads(res.content)
     values = data["data"]["result"][0]["values"]
     return values
+
 
 def find_current_value(predict, conf_int):
     now = datetime.datetime.now()
@@ -47,10 +55,34 @@ def find_current_value(predict, conf_int):
             return k, v, conf_int[i]
         i += 1
 
+
 def to_exporter_metrics(d):
-    return ""
+    ret = []
+    for metric_name, data in d.items():
+        ret.append('promad_predict_max{name=\"%s\"} %f' % (
+            metric_name, data["predict"]["range_max"]))
+        ret.append('promad_predict_min{name=\"%s\"} %f' % (
+            metric_name, data["predict"]["range_min"]))
+        ret.append('promad_predict{name=\"%s\"} %f' %
+                   (metric_name, data["predict"]["value"]))
+        ret.append('promad_actual{name=\"%s\"} %s' %
+                   (metric_name, data["actual"]["value"]))
+        ret.append('promad_error_rate{name=\"%s\"} %f' % (metric_name, abs((float(
+            data["actual"]["value"]) - data["predict"]["value"])/data["predict"]["value"])))
+        ret.append('promad_anomaly_detect{name=\"%s\"} %f' % (
+            metric_name, data["actual"]["out_of_range"]))
+    return "\n".join(ret)
+
 
 def parse_rules():
     with open(os.environ.get("RULES_PATH")) as f:
-      return yaml.safe_load(f)
-    
+        return yaml.safe_load(f)
+
+
+def fit_model():
+    return {v["name"]: pm.ARIMA(
+        order=literal_eval(v["arima"]["order"]),
+        seasonal_order=literal_eval(v["arima"]["seasonal_order"])
+    ).fit(
+        y=val_to_series(query_range(v["query"]))
+    ) for v in parse_rules()}
--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 from fastapi import FastAPI
-import pmdarima as pm
-import datetime
+from fastapi.responses import PlainTextResponse
 
 import lib.lib as lib
 
 app = FastAPI()
-print(lib.parse_rules())
-order, seasonal_order = lib.arima_orders()
-#models = {v["name"]: pm.ARIMA(order=order, seasonal_order=seasonal_order).fit(
-#    lib.query_range(v["query"])) for v in lib.parse_rules()}
-models = {v["name"]: pm.ARIMA(order=order, seasonal_order=seasonal_order).fit(y=lib.val_to_series(lib.query_range(v["query"]))) for v in lib.parse_rules()}
+
+app.models = lib.fit_model()
 
 
 @app.get("/")
 async def root():
-    return {"message": "hello"}
+    return {"metrics": list(app.models.keys())}
 
 
-@app.get("/metrics")
+@app.post("/fit")
+async def fit():
+    app.models = lib.fit_model()
+    return {"metrics": list(app.models.keys())}
+
+
+@app.get("/metrics", response_class=PlainTextResponse)
 async def metrics():
-    predict, conf_int = models["pod_cpu"].predict(n_periods=12, return_conf_int=True)
-    predict_time, predict_value, ci = lib.find_current_value(predict, conf_int)
-    value = lib.query_current()
-    return {
-        "predict": {
-            "time": predict_time,
-            "value": predict_value,
-            "range_max": ci[1],
-            "range_min": ci[0]
-        },
-        "actual": {
-            "time": datetime.datetime.fromtimestamp(value[0]),
-            "value": value[1],
-            "in_range": 1 if ci[0] < float(value[1]) < ci[1] else 0
+    ret = {}
+    for k, model in app.models.items():
+        predict, conf_int = model.predict(
+            n_periods=12, return_conf_int=True)
+        _, predict_value, ci = lib.find_current_value(predict, conf_int)
+        value = lib.query_current()
+        ret[k] = {
+            "predict": {
+                "value": predict_value,
+                "range_max": ci[1],
+                "range_min": ci[0]
+            },
+            "actual": {
+                "value": value[1],
+                "out_of_range": 0 if ci[0] < float(value[1]) < ci[1] else 1
+            }
         }
-    }
+    return lib.to_exporter_metrics(ret)
